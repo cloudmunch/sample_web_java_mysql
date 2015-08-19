65fb2e46e4bbfe597d6d49c0376457c9c5483d46
cmPrasanna <prasanna@cloudmunch.com>
2015-08-19 17:45:58 +0530

    Update HelloWorldResource.java

--- src/main/java/com/cloudmunch/javademo/HelloWorldResource.java
+++ src/main/java/com/cloudmunch/javademo/HelloWorldResource.java
@@ -18,7 +18,7 @@ public class HelloWorldResource {
 		logger.info("sample info message");
 		logger.warn("sample warning message");
 		logger.error("sample error message");
-		return "Welcome to a SO GOOD CloudMunch Demo Aug 18, 2015";
+		return "Welcome to a VERY GOOD CloudMunch Demo Aug 19, 2015";
 	}
 
 }
