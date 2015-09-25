e298d1503a5722f848aea9da3c3640a54c31f040
talimerenl <talimeren.longkumer@wipro.com>
2015-09-25 16:03:05 +0530

    Update HelloWorldResource.java
    changed line 21

--- src/main/java/com/cloudmunch/javademo/HelloWorldResource.java
+++ src/main/java/com/cloudmunch/javademo/HelloWorldResource.java
@@ -18,7 +18,7 @@ public class HelloWorldResource {
 		logger.info("sample info message");
 		logger.warn("sample warning message");
 		logger.error("sample error message");
-		return "Welcome to  DevOps Demo Aug 19, 2015";
+		return "Welcome to Wipro DevOps Demo 2015";
 	}
 
 }
